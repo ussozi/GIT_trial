d6a2d2fe6913474c06c496504dc31d38d9e51a25
ussozi <45781527+ussozi@users.noreply.github.com>
2020-08-12 10:12:51 +0300

    Add files via upload

---
+++ add_numbers.py
@@ -0,0 +1,18 @@
+# Program to add two numbers
+
+def add_numbers(a,b):
+    z=a+b
+    return z
+add_numbers(5, 5)
+
+add_numbers(15, 5)
+
+!git add add_numbers.py
+!git commit -m "My commit 1"
+!git remote add origin https://github.com/ussozi/GIT_trial.git
+
+!git pull origin master
+
+!git push -u origin master
+
+!git push origin master
